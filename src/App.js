36695e591e17264acd02a6ccc204d1eaa90bf6c4
Beckyab <rebeccaabutu46@gmail.com>
2022-10-15 22:02:26 +0100

    Second commit

--- src/App.js
+++ src/App.js
@@ -317,15 +317,11 @@ function App() {
               </div>
               <div className="col-md-8">
                 <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">
-                    This is a wider card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit
-                    longer.
-                  </p>
+                  <h5 className="card-title">Shortlet Apartment for Lodgers</h5>
+                
                   <p className="card-text">
                     <small className="text-muted">
-                      Last updated 3 mins ago
+                      Last updated 5 mins ago
                     </small>
                   </p>
                 </div>
