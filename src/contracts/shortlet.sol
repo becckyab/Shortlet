99a0b45fec6b748c73776e43bea7fbbb074506b9
KishoreVB70 <kishore07kzr@gmail.com>
2022-10-20 11:40:05 +0530

    made improvements

--- src/contracts/shortlet.sol
+++ src/contracts/shortlet.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT 
- 
+import "@openzeppelin/contracts/utils/Counters.sol";
 pragma solidity >=0.7.0 <0.9.0; 
  
 interface IERC20Token { 
@@ -29,7 +29,10 @@ interface IERC20Token {
  
 //Shortlet is modeled in the formart of a short stay apartment which can be used in place of hotels 
  
-contract houseShortlet { 
+contract HouseShortlet { 
+
+    using Counters for Counters.Counter;
+    Counters.Counter shortletLength;
  
     struct Shortlet { 
         address payable owner; 
@@ -39,15 +42,13 @@ contract houseShortlet {
         string wifi; 
         uint numberOfBedroom; 
         uint256 amount; 
-        uint256 likes; 
+        uint256 likes;
     } 
  
- 
+    mapping(uint => mapping(address => bool)) hasLiked;
     mapping(uint256 => Shortlet) shortlets; 
-    uint256 shortletLength = 0; 
  
- 
-    address internal cUsdTokenAddress = 
+    address internal constant cUsdTokenAddress = 
         0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1; 
  
 //add function for the shortlet 
@@ -60,7 +61,7 @@ contract houseShortlet {
         uint _numberOfBedroom, 
         uint256 _amount 
     ) public { 
-        shortlets[shortletLength] = Shortlet( 
+        shortlets[shortletLength.current()] = Shortlet( 
             payable(msg.sender), 
             _typeOfApartment, 
             _image, 
@@ -70,9 +71,45 @@ contract houseShortlet {
             _amount, 
             0 
         ); 
- 
-        shortletLength++; 
+        shortletLength.increment(); 
     } 
+ 
+ 
+    //a function to rent a shortlet apartment 
+ 
+    function buyShortlet(uint256 _index) public payable { 
+        require( 
+            IERC20Token(cUsdTokenAddress).transferFrom( 
+                msg.sender, 
+                shortlets[_index].owner, 
+                shortlets[_index].amount 
+            ), 
+            "Transaction could not be performed" 
+        ); 
+        shortlets[_index].owner = payable(msg.sender); 
+    } 
+
+//A function for users to like the apartments of their choice 
+ 
+    function likeShortlet(uint256 _index) public { 
+        require(msg.sender != shortlets[_index].owner, "owner can't like");
+        require(!hasLiked[_index][msg.sender], "already liked");
+        hasLiked[_index][msg.sender] = true;
+
+        shortlets[_index].likes++; 
+        if (shortlets[_index].likes % 2 == 0) { 
+            shortlets[_index].amount++; 
+        } 
+    } 
+ 
+ 
+    //A function for users to rate the apartments of their choice 
+ 
+    function rateShortlet(uint _index) public { 
+        require(msg.sender == shortlets[_index].owner, "only buyer can rate");
+        shortlets[_index] = shortlets[shortletLength.current() - 1]; 
+    } 
+
  
 //get function for te short let apartments 
  
@@ -102,38 +139,7 @@ contract houseShortlet {
             shortlet.likes 
         ); 
     } 
- 
-//A function for users to like the apartments of their choice 
- 
-    function likeShortlet(uint256 _index) public { 
-        shortlets[_index].likes++; 
-        if (shortlets[_index].likes % 2 == 0) { 
-            shortlets[_index].amount++; 
-        } 
-    } 
- 
-    //a function to rent a shortlet apartment 
- 
-    function buyShortlet(uint256 _index) public payable { 
-        require( 
-            IERC20Token(cUsdTokenAddress).transferFrom( 
-                msg.sender, 
-                shortlets[_index].owner, 
-                shortlets[_index].amount 
-            ), 
-            "Transaction could not be performed" 
-        ); 
-        shortlets[_index].owner = payable(msg.sender); 
-    } 
- 
-    //A function for users to rate the apartments of their choice 
- 
-    function rateShortlet(uint _index) public { 
-            require(msg.sender == shortlets[_index].owner, "Invalid owner"); 
-            shortlets[_index] = shortlets[shortletLength - 1]; 
-    } 
- 
     function getShortletLength() public view returns (uint256) { 
-        return (shortletLength); 
+        return (shortletLength.current()); 
     } 
 }
